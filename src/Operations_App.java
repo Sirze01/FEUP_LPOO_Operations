293cf4210d8495370adb017b9eb56d94976f41cf
Tatiana Fritzová <fritzova@inqool.cz>
2021-02-17 12:06:58 +0100

    added support for multiple operations

--- src/Operations_App.java
+++ src/Operations_App.java
@@ -5,9 +5,29 @@ public class Operations_App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String operation = scanner.nextLine();
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(a + b);
+        int result;
+        switch (operation) {
+            case "sum":
+                result = a + b;
+                break;
+            case "sub":
+                result = a - b;
+                break;
+            case "mul":
+                result = a * b;
+                break;
+            case "div":
+                result = a / b;
+                break;
+            default:
+                throw new RuntimeException("unknown operation");
+        }
+
+        System.out.println(result);
     }
 }
